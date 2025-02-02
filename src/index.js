6187cc1abb180596e176fccb5acf635b723161fa
behole <behole@gmail.com>
2025-02-02 10:43:12 -0800

    Add src/index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PokerTrainingApp from './components/PokerTrainingApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <PokerTrainingApp />
+  </React.StrictMode>
+);
