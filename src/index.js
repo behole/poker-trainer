d7ab41cf0f6b92e2689a53ee8c2488a79640f399
behole <behole@gmail.com>
2025-02-02 12:54:03 -0800

    Update index.js with test component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import PokerTrainingApp from './components/PokerTrainingApp';
 
+// Test element to ensure React is loading
+const TestApp = () => {
+  return (
+    <div style={{ padding: '20px', color: 'white' }}>
+      <h1>Poker Trainer Test</h1>
+      <p>If you can see this, React is working!</p>
+      <PokerTrainingApp />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PokerTrainingApp />
+    <TestApp />
   </React.StrictMode>
 );
