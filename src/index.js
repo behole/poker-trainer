38156c371145a0ad389d7642d46f3adeaa9677b8
behole <behole@gmail.com>
2025-02-02 13:05:57 -0800

    Add CSS import to index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.css';
 import PokerTrainingApp from './components/PokerTrainingApp';
 
-// Get the root element
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error('Failed to find the root element');
-}
-
-// Create a root
 const root = createRoot(rootElement);
 
-// Render your app
 root.render(
   <React.StrictMode>
     <PokerTrainingApp />
